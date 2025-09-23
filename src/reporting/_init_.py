cc80b7ab3c5f8675b4c2c4a8ef67c364f4fba762
Sujoy <iamsujoy004@gmail.com>
2025-09-23 23:44:55 +0530

    Update _init_.py

--- src/reporting/_init_.py
+++ src/reporting/_init_.py
@@ -1,1 +1,23 @@
+"""
+Reporting Package
 
+This package contains report generation modules:
+- PDF Report Generator: pdf_report.py
+- Weekly Digest: weekly_digest.py (future)
+
+Usage:
+    from src.reporting.pdf_report import generate_chat_analysis_pdf
+"""
+
+from .pdf_report import (
+    generate_chat_analysis_pdf,
+    ChatAnalysisPDFGenerator
+)
+
+__all__ = [
+    'generate_chat_analysis_pdf',
+    'ChatAnalysisPDFGenerator'
+]
+
+__version__ = '1.0.0' 
+__author__ = 'Chat Analyzer Pro Team'
