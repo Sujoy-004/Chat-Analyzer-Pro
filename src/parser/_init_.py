c79d93c0241da1fdcad3845b8bd8351f032306d3
Sujoy <iamsujoy004@gmail.com>
2025-09-23 23:43:02 +0530

    Update _init_.py

--- src/parser/_init_.py
+++ src/parser/_init_.py
@@ -1,1 +1,22 @@
+"""
+Chat Parser Package
 
+This package contains parsers for different chat platforms:
+- WhatsApp parser: whatsapp_parser.py
+- Telegram parser: telegram_parser.py
+
+Usage:
+    from src.parser.whatsapp_parser import parse_whatsapp_chat
+    from src.parser.telegram_parser import parse_telegram_chat
+"""
+
+from .whatsapp_parser import parse_whatsapp_chat
+from .telegram_parser import parse_telegram_json
+
+__all__ = [
+    'parse_whatsapp_chat',
+    'parse_telegram_json'
+]
+
+__version__ = '1.0.0'
+__author__ = 'Chat Analyzer Pro Team'
