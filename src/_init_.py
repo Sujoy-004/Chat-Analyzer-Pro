34a2ba4e2f16c83f425b79c2a1a60412b3a07a35
Sujoy <iamsujoy004@gmail.com>
2025-10-09 06:54:23 +0530

    Update _init_.py

--- src/_init_.py
+++ src/_init_.py
@@ -1,1 +1,22 @@
+"""
+Chat Analyzer Pro - Main Package
+Complete chat analysis suite with NLP, visualization, and gamification.
+"""
 
+__version__ = "1.0.0"
+__author__ = "Sujoy"
+__project__ = "Chat Analyzer Pro"
+
+# Package metadata
+__all__ = [
+    'parser',
+    'analysis',
+    'reporting',
+    'utils'
+]
+
+# Import subpackages
+from . import parser
+from . import analysis
+from . import reporting
+from . import utils
